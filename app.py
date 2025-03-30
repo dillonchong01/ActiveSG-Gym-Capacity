6206951c705d8528b5c2fd5c6c4f528c287baff1
Dillon Chong <124565587+dillonchong01@users.noreply.github.com>
2025-03-30 19:25:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,3 @@ def home():
 @app.route('/graphs/<filename>')
 def get_graph(filename):
     return send_from_directory('static/graphs', filename)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    print(f"==> Starting Flask on port {port}")
-    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
