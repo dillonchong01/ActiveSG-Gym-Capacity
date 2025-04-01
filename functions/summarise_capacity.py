f28c8f610a20258279fa27758a10c2bedb0e02b3
Dillon Chong <124565587+dillonchong01@users.noreply.github.com>
2025-04-01 10:21:47 +0800

    Update summarise_capacity.py

--- functions/summarise_capacity.py
+++ functions/summarise_capacity.py
@@ -1,5 +1,32 @@
 import sqlite3
 import pandas as pd
+from pathlib import Path
+
+DB_PATH = Path("database/gym_capacity.db")
+SUMMARY_DB_PATH = Path("database/gym_capacity_summary.db")
 
 def summarize_capacity():
+    # Connect to local SQLite database
+    conn = sqlite3.connect(DB_PATH)
+
+    # Load raw data
+    query = "SELECT gym_name, capacity, date, time FROM gym_capacity;"
+    df = pd.read_sql(query, conn)
+    conn.close()
+
+    if df.empty:
+      return
+
+    # Group by gym_name and time and obtain average capacity
+    df_grouped = df.groupby(["gym_name", "time"])["capacity"].mean().reset_index()
+    df_grouped["gym_name"] = df_grouped["gym_name"].apply(lambda x: re.sub(r'\b(ActiveSG|Gym)\b|@', '', x).strip())
+    df_grouped["time"] = df_grouped["time"].apply(lambda t: datetime.strptime(t, "%H:%M"))
+
+    # Save into summary database
+    summary_conn = sqlite3.connect(SUMMARY_DB_PATH)
+    summary_df.to_sql("gym_capacity_summary", summary_conn, if_exists="replace", index=False)
+    summary_conn.close()
+
+if __name__ == "__main__":
+    summarize_capacity()
   
